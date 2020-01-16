a8a2c976b04f7b29cafc08a80f2547ea70f11560
rowanho <rowanho45@gmail.com>
2020-01-16 13:16:30 +0000

    Uses fingerprints not strings

--- invertedindex/invertedindex.go
+++ invertedindex/invertedindex.go
@@ -1,16 +1,10 @@
 package invertedindex
 
-import (
-	"fmt"
-	"regexp"
-	"strings"
-)
-
 // InvertedIndexEntry contains the term followed by the
 // number of times it has appeared across all documents
 // and an array of documents it is persent in
 type InvertedIndexEntry struct {
-	Term            string
+	Term            uint64
 	Frequency       int
 	DocumentListing []int
 }
@@ -18,19 +12,20 @@ type InvertedIndexEntry struct {
 // InvertedIndex contains a hash map to easily check if the
 // term is present and an array of InvertedIndexEntry
 type InvertedIndex struct {
-	HashMap map[string]*InvertedIndexEntry
+	HashMap map[uint64]*InvertedIndexEntry
 	Items   []*InvertedIndexEntry
 }
 
 // FindItem returns the position of a given
 // Item in an Inverted Index
-func (invertedIndex *InvertedIndex) FindItem(Term string) int {
+func (invertedIndex *InvertedIndex) FindItem(Term uint64) []int {
+	indexes := make([]int, 0)
 	for index, item := range invertedIndex.Items {
 		if item.Term == Term {
-			return index
+			indexes = append(indexes, index)
 		}
 	}
-	panic("Not Found")
+	return indexes
 }
 
 // AddItem works by first checking if a given term is already present
@@ -38,7 +33,7 @@ func (invertedIndex *InvertedIndex) FindItem(Term string) int {
 // present it updates the Items by increasing the frequency and
 // adding the document it is found in. If it is not present it
 // adds it to the hash map and adds it to the items list
-func (invertedIndex *InvertedIndex) AddItem(Term string, Document int) {
+func (invertedIndex *InvertedIndex) AddItem(Term uint64, Document int) {
 	if invertedIndex.HashMap[Term] != nil {
 		// log.Println("Index item", Term, "already exists :: updating existing item")
 
@@ -64,70 +59,23 @@ func (invertedIndex *InvertedIndex) AddItem(Term string, Document int) {
 // empty Inverted Index
 func CreateInvertedIndex() *InvertedIndex {
 	invertedIndex := &InvertedIndex{
-		HashMap: make(map[string]*InvertedIndexEntry),
+		HashMap: make(map[uint64]*InvertedIndexEntry),
 		Items:   []*InvertedIndexEntry{},
 	}
 	return invertedIndex
 }
 
-// RemoveDuplicates filters out all duplicate
-// words from each document
-func RemoveDuplicates(wordList []string) []string {
-	keys := make(map[string]bool)
-	uniqueWords := []string{}
 
-	for _, entry := range wordList {
-		if _, value := keys[entry]; !value {
-			keys[entry] = true
-			uniqueWords = append(uniqueWords, entry)
+func GenerateFpMap(fps []uint64) map[uint64]bool {
+	fpMap := make(map[uint64]bool)
+
+	for _, fp := range fps {
+		if _, value := fpMap[fp]; !value {
+			fpMap[fp] = true
 		}
 	}
 
-	return uniqueWords
-}
-
-// Preprocessing converts each word to lowercase
-// TODO: Clean up each word for symbols
-func Preprocessing(wordList []string) []string {
-	ProcessedWordList := []string{}
-
-	// Convert each string to lowercase from
-	// wordList and add to ProcessedWordList
-	for _, word := range wordList {
-		ProcessedWordList = append(ProcessedWordList, strings.ToLower(word))
-	}
-
-	return ProcessedWordList
-}
-
-// Tokenize gets the individual words from each
-// document and generates a wordlist
-func Tokenize(Doc string) []string {
-	wordList := []string{}
-
-	// The following regexp finds individual
-	// words in a sentence
-	r := regexp.MustCompile("[^\\s]+")
-	wordList = r.FindAllString(Doc, -1)
-
-	wordList = Preprocessing(wordList)
-	wordList = RemoveDuplicates(wordList)
-
-	return wordList
-}
-
-// GenerateDocMap creates a hash map of
-// each word in the document
-func GenerateDocMap(token []string) map[string]bool {
-	docMap := make(map[string]bool)
-
-	for _, word := range token {
-		if _, value := docMap[word]; !value {
-			docMap[word] = true
-		}
-	}
-
-	return docMap
+	return fpMap
 }
 
 // GenerateInvertedIndex for each document list
@@ -135,13 +83,12 @@ func GenerateDocMap(token []string) map[string]bool {
 // generates a hash map for each document
 // using them it then generates the
 // inverted index of all words
-func GenerateInvertedIndex(DocList []string) InvertedIndex {
-	globalDocMap := make([]map[string]bool, 0)
+func GenerateInvertedIndex(fpList [][]uint64) InvertedIndex {
+	fpMaps := make([]map[uint64]bool, len(fpList))
 
-	for _, Doc := range DocList {
-		token := Tokenize(Doc)
-		docMap := GenerateDocMap(token)
-		globalDocMap = append(globalDocMap, docMap)
+	for _, fps := range fpList {
+		fpMap := GenerateFpMap(fps)
+		fpMaps = append(fpMaps, fpMap)
 	}
 
 	// Create an empty inverted index
@@ -149,27 +96,10 @@ func GenerateInvertedIndex(DocList []string) InvertedIndex {
 
 	// Using the generated hash maps add
 	// each word to the inverted index
-	for DocMapIndex, DocMap := range globalDocMap {
-		for DocEntry := range DocMap {
-			invertedIndex.AddItem(DocEntry, DocMapIndex)
+	for index, fpMap := range fpMaps {
+		for fp := range fpMap {
+			invertedIndex.AddItem(fp, index)
 		}
 	}
 	return *invertedIndex
-}
-
-// Find for a given inverted index and search term
-// checks if the term exists and then
-// outputs the documents the
-// term is in
-func Find(index InvertedIndex, searchTerm string) {
-	Term := strings.ToLower(searchTerm)
-
-	if index.HashMap[Term] != nil {
-		itemPosition := index.FindItem(Term)
-		item := index.Items[itemPosition]
-
-		fmt.Println("Found:", searchTerm, "in documents:", item.DocumentListing)
-	} else {
-		fmt.Println("Not Found:", searchTerm)
-	}
 }
